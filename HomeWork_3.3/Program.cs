a051a00de4db6ecb67b7dbdd9d050025a59c20cb
Alex <spider_inter@mail.ru>
2023-04-07 14:40:02 +0400

    Третье задание третьего урока

---
+++ HomeWork_3.3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+ Console.Write("Введите Число N ");
+ int num = Convert.ToInt32(Console.ReadLine());
+ int x = GetNumber(num);
+
+  int GetNumber(int num){
+int i = 1;
+while (i < num) {  
+  int Result = (int)Math.Pow(i, 3);
+   Console.Write($"{Result}{";"}");
+  i++;
+}
+return num;
+}
