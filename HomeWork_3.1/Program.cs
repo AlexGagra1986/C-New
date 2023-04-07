7a9b3d28bd8279f982aeb6e616d423df695bd9e5
Alex <spider_inter@mail.ru>
2023-04-07 14:32:09 +0400

    Первое задание третьего урока

---
+++ HomeWork_3.1/Program.cs
@@ -0,0 +1,35 @@
+﻿       Console.Write("--> Введите число : ");
+       int num = Convert.ToInt32(Console.ReadLine());
+       Console.Write($"Число {num} = {IsPalindrome(num)}  ");
+  
+          
+
+
+ static bool IsPalindrome(int num)
+        {
+            if (num >= 0 && num <10) /// Если число двухзначное возвращаем истину
+                return true;
+            int numLength = GetLength(num); /// получаем длинну числа
+            int[] digits = new int[numLength]; ///  ложим в массив число
+            for (int i = numLength - 1; i >= 0; i--) {
+                digits[i] = num % 10;
+                num /= 10;
+            }
+            for (int i = 0; i < numLength/2; i++) {
+                if (digits[i] != digits[numLength - i - 1])
+                    return false;
+            }
+            return true;
+        }
+        
+        static int GetLength(int num)
+        {
+            int n = 0;
+            while (num > 0) {
+                num /= 10;
+                n++;
+            }
+            return n;
+        }
+    
+
