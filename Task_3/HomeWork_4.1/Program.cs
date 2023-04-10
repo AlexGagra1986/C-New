d42d9479ec91b365f59eecb854b4b678d58f1eb1
aleksandrobuhov <spider_inter@mail.ru>
2023-04-10 19:09:07 +0400

    Первое задание 4 урока

---
+++ Task_3/HomeWork_4.1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("--> Введите число A : ");
+       int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("--> Введите число B : ");   
+ int b = Convert.ToInt32(Console.ReadLine());  
+expo(a,b);
+
+int expo(int a, int b){
+
+int num = 1;
+
+for (int i = 0; i < b; i++) {
+        num *= a;
+Console.Write($"--> {num} ");
+    }
+    return num;
+}
+
