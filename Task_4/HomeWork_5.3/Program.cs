11eff1d9d46b60290301a7f1e5ec97b60fcfc4ae
aleksandrobuhov <spider_inter@mail.ru>
2023-04-11 16:32:24 +0400

    3 задание 5 урока

---
+++ Task_4/HomeWork_5.3/Program.cs
@@ -0,0 +1,10 @@
+﻿
+double[] array = {2,3,78,7,22};
+double min = array[0];
+double max = 0;
+for(int i = 0;i < array.Length;i++){
+if(array[i]>max){ max = array[i];}
+if(array[i]<min){ min = array[i];}
+}
+double result = max-min;
+Console.Write(result);
