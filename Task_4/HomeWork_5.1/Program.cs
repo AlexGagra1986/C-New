a7afcb3499f31d2936e5feb8ae869af8fc07ed20
aleksandrobuhov <spider_inter@mail.ru>
2023-04-11 15:29:35 +0400

    Первое задание 5 урока

---
+++ Task_4/HomeWork_5.1/Program.cs
@@ -0,0 +1,11 @@
+﻿
+int [] array = {345,897,568,234};
+int count = 0;
+int chek = 0;
+for (int i = 0;i < array.Length;i++){
+    if(array[i]%2==0){
+        chek++;
+        }
+
+}
+Console.Write(chek);
