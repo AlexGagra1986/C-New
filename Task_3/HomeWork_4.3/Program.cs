f5d900a7addad98cf406ea016e2296905f576819
aleksandrobuhov <spider_inter@mail.ru>
2023-04-11 14:57:01 +0400

    третье задание 4 урока

---
+++ Task_3/HomeWork_4.3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("--> Введите числа массива ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+int[] array = new int[x.ToString().Length];
+for(int i = 0;i < array.Length;i++){
+array[i] = x%10;
+x/=10;  
+}
+Array.Reverse(array);
+
+for(int j = 0;j < array.Length;j++){
+    Console.Write($"[{array[j]}]");
+}
+
+
