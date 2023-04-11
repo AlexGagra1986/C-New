af3cd64332d9dc5b796744826de43f44506233a4
aleksandrobuhov <spider_inter@mail.ru>
2023-04-11 14:23:47 +0400

    второе задание 4 урока

---
+++ Task_3/HomeWork_4.2/Program.cs
@@ -0,0 +1,19 @@
+﻿///Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+///452 -> 11
+Console.Write("--> Введите число N : ");
+       int x = Convert.ToInt32(Console.ReadLine());
+
+int a = 0;
+int sum = 0;
+while (x>0){
+ a  = x % 10;
+ sum = sum+a;
+ x/=10;
+}
+  Console.Write(sum); 
+
+    
+
+
+
+
