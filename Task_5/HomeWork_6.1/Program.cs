9a2b425030cd2506b0ac8bca1a9af055389ed743
aleksandrobuhov <spider_inter@mail.ru>
2023-04-12 08:55:14 +0400

    Первое задание 6 урока

---
+++ Task_5/HomeWork_6.1/Program.cs
@@ -0,0 +1,21 @@
+﻿
+fs();
+int fs(){
+    Console.WriteLine("Введите количество цифр");
+    int m = Convert.ToInt32(Console.ReadLine());
+    int i = 0;
+    int count = 0;
+    while (m>i)
+    {  
+        i++;
+        Console.Write("Введите Число ");
+        int x = Convert.ToInt32(Console.ReadLine());
+       
+        if(x>0){
+            count++;
+          }
+    }
+Console.WriteLine($"Количество положительных чисел {count}");
+    return 0;
+}
+
