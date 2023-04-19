76bd32147d70a0e623435315c1efa525cdee068c
aleksandrobuhov <spider_inter@mail.ru>
2023-04-19 17:50:05 +0400

    7 такс 1 задание

---
+++ Task_7/HomeWork_1/Program.cs
@@ -0,0 +1,38 @@
+﻿int N = RetMessange("Введите N "); 
+int M = RetMessange("Введите M "); 
+AddArray(M, N);
+
+int RetMessange(String msg){
+ Console.Write(msg);
+int  x = Convert.ToInt32(Console.ReadLine());
+return x;
+}
+
+void AddArray(int M,int N){
+double [,] array = new double[M, N];
+for (int i = 0; i < M ; i++)
+{ 
+    Random x = new Random();
+    for (int j = 0; j < N ;j++)
+    {
+        array[i,j] = x.Next(-15, 15);
+        Console.Write("{0}\t",array[i, j]);
+    }
+      Console.WriteLine("");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
